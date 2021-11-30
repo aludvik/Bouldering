a75b469d68d1d1245ce87586a9d556b6afa5e524
Adam M Ludvik <adam.m.ludvik@gmail.com>
2021-11-30 14:22:32 -0600

    Record states with all tractor positions
    This eliminates duplicate found states where the only difference is the
    position of the tractor within a constant reachable set of positions.

--- lvlgen/src/main.rs
+++ lvlgen/src/main.rs
@@ -1,28 +1,35 @@
 use std::io::{self, Read};
 use std::collections::hash_set::HashSet;
+use std::collections::hash_map::HashMap;
 
 fn main() -> io::Result<()> {
   let mut stdin = io::stdin();
-  let grid = read_grid(&mut stdin)?;
-  let width = guess_width(grid.len()).unwrap();
-  let tractor = find_tractor(&grid).unwrap();
-  let found = search_states(tractor, grid, width);
+  let state = read_game_state(&mut stdin)?;
+  let width = guess_width(state.grid.len()).unwrap();
+  let found = search_states(state, width);
   for state in found {
     print_state(&state, width);
   }
   Ok(())
 }
 
-fn read_grid<T: Read>(input: &mut T) -> io::Result<Vec<Cell>> {
+fn read_game_state<T: Read>(input: &mut T) -> io::Result<GameState> {
   let mut buffer = String::new();
   input.read_to_string(&mut buffer)?;
   let mut grid = vec![];
   for c in buffer.chars() {
     if let Some(cell) = Cell::try_from_char(c) {
       grid.push(cell);
+    } else if c != '\n' {
+      panic!("unrecognized character `{}`", c);
     }
   }
-  Ok(grid)
+  if let Some(tractor) = find_tractor(&grid) {
+    grid[tractor] = Cell::Empty;
+    Ok(GameState { grid, tractor })
+  } else {
+    panic!("invalid input, no tractor");
+  }
 }
 
 fn find_tractor(grid: &Vec<Cell>) -> Option<usize> {
@@ -79,7 +86,7 @@ impl Cell {
   pub fn try_from_char(c: char) -> Option<Self> {
     Some(match c {
       ' ' => Cell::Empty,
-      't' => Cell::Tractor,
+      '.' => Cell::Tractor,
       '@' => Cell::BoulderInHole,
       'O' => Cell::Hole,
       '#' => Cell::Block,
@@ -90,7 +97,7 @@ impl Cell {
   pub fn to_char(&self) -> char {
     match self {
       Cell::Empty => ' ',
-      Cell::Tractor => 't',
+      Cell::Tractor => '.',
       Cell::BoulderInHole => '@',
       Cell::Hole => 'O',
       Cell::Block => '#',
@@ -103,9 +110,12 @@ fn to_index(row: usize, col: usize, width: usize) -> usize {
   row * width + col
 }
 
-fn grid_to_movement_graph(grid: &Vec<Cell>, board_size: usize) -> Vec<Vec<usize>> {
-  let mut graph = vec![];
+fn grid_to_movement_graph(grid: &Vec<Cell>, board_size: usize) -> HashMap<usize, Vec<usize>> {
+  let mut graph = HashMap::new();
   for idx in 0..grid.len() {
+    if grid[idx] != Cell::Empty {
+      continue;
+    }
     let mut edges = vec![];
     let row = idx / board_size;
     let col = idx % board_size;
@@ -133,17 +143,17 @@ fn grid_to_movement_graph(grid: &Vec<Cell>, board_size: usize) -> Vec<Vec<usize>
         edges.push(right);
       }
     }
-    graph.push(edges);
+    graph.insert(idx, edges);
   }
   graph
 }
 
-fn walk_graph_from(from: usize, graph: &Vec<Vec<usize>>) -> HashSet<usize> {
+fn walk_graph_from(from: usize, graph: &HashMap<usize, Vec<usize>>) -> HashSet<usize> {
   let mut visited = HashSet::new();
   visited.insert(from);
   let mut stack = vec![from];
   while let Some(current) = stack.pop() {
-    for neighbor in &graph[current] {
+    for neighbor in &graph[&current] {
       if !visited.contains(neighbor) {
         visited.insert(*neighbor);
         stack.push(*neighbor);
@@ -217,18 +227,16 @@ fn is_extendable(boulder: usize, dir: Direction, grid: &Vec<Cell>, board_size: u
 fn extend_branch(state: &GameState, boulder: usize, dir: Direction, board_size: usize) -> GameState {
   let mut new_grid = state.grid.clone();
   assert!(new_grid[boulder] == Cell::Boulder || new_grid[boulder] == Cell::BoulderInHole);
-  assert!(new_grid[state.tractor] == Cell::Tractor);
+  assert!(new_grid[state.tractor] == Cell::Empty);
   if new_grid[boulder] == Cell::Boulder {
     new_grid[boulder] = Cell::Empty;
   }
   if new_grid[boulder] == Cell::BoulderInHole {
     new_grid[boulder] = Cell::Hole;
   }
-  new_grid[state.tractor] = Cell::Empty;
   let new_boulder = move_one(boulder, dir, board_size).unwrap();
   let new_tractor = move_one(new_boulder, dir, board_size).unwrap();
   new_grid[new_boulder] = Cell::Boulder;
-  new_grid[new_tractor] = Cell::Tractor;
   GameState { grid: new_grid, tractor: new_tractor }
 }
 
@@ -237,16 +245,29 @@ struct GameState {
   pub tractor: usize,
 }
 
-fn search_states(tractor: usize, grid: Vec<Cell>, board_size: usize) -> HashSet<Vec<Cell>> {
+fn search_states(state: GameState, board_size: usize) -> HashSet<Vec<Cell>> {
   let mut found = HashSet::new();
-  let state = GameState { grid, tractor };
   search(state, board_size, &mut found);
   found
+}
+
+fn fill_reachable_with_tractors(grid: &Vec<Cell>, reachable: &HashSet<usize>) -> Vec<Cell> {
+  let mut new_grid: Vec<Cell> = grid.clone();
+  for idx in reachable {
+    assert!(new_grid[*idx] == Cell::Empty);
+    new_grid[*idx] = Cell::Tractor;
+  }
+  new_grid
 }
 
 fn search(state: GameState, board_size: usize, found: &mut HashSet<Vec<Cell>>) {
   let graph = grid_to_movement_graph(&state.grid, board_size);
   let reachable = walk_graph_from(state.tractor, &graph);
+  let filled_state = fill_reachable_with_tractors(&state.grid, &reachable);
+  if found.contains(&filled_state) {
+    return;
+  }
+  found.insert(filled_state);
   for (idx, cell) in state.grid.iter().enumerate() {
     if cell != &Cell::Boulder && cell != &Cell::BoulderInHole {
       continue;
@@ -255,12 +276,7 @@ fn search(state: GameState, board_size: usize, found: &mut HashSet<Vec<Cell>>) {
       if !is_extendable(idx, *dir, &state.grid, board_size, &reachable) {
         continue;
       }
-      let new_state = extend_branch(&state, idx, *dir, board_size);
-      if found.contains(&new_state.grid) {
-        continue;
-      }
-      found.insert(new_state.grid.clone());
-      search(new_state, board_size, found);
+      search(extend_branch(&state, idx, *dir, board_size), board_size, found);
     }
   }
 }
@@ -274,11 +290,10 @@ mod test {
     let grid = vec![
       Cell::BoulderInHole, Cell::Empty, Cell::Empty, Cell::BoulderInHole,
       Cell::Empty, Cell::Empty, Cell::Empty, Cell::Empty,
-      Cell::Tractor, Cell::Empty, Cell::Empty, Cell::Empty,
+      Cell::Empty, Cell::Empty, Cell::Empty, Cell::Empty,
       Cell::BoulderInHole, Cell::Empty, Cell::Empty, Cell::BoulderInHole,
     ];
-    let mut found = HashSet::new();
-    search(8, grid, 4, &mut found);
+    let found = search_states(GameState { tractor: 8, grid }, 4);
   }
 
   #[test]
@@ -302,6 +317,30 @@ mod test {
   }
 
   #[test]
+  fn test_walk_movement_graph_2() {
+    let grid = vec![
+      Cell::Hole, Cell::Empty, Cell::Empty, Cell::BoulderInHole,
+      Cell::Boulder, Cell::Empty, Cell::Empty, Cell::Empty,
+      Cell::Empty, Cell::Empty, Cell::Empty, Cell::Empty,
+      Cell::BoulderInHole, Cell::Empty, Cell::Empty, Cell::BoulderInHole,
+    ];
+    let graph = grid_to_movement_graph(&grid, 4);
+    let reachable = walk_graph_from(8, &graph);
+    assert_eq!(reachable.len(), 11);
+    assert!(reachable.contains(&1));
+    assert!(reachable.contains(&2));
+    assert!(reachable.contains(&5));
+    assert!(reachable.contains(&6));
+    assert!(reachable.contains(&7));
+    assert!(reachable.contains(&8));
+    assert!(reachable.contains(&9));
+    assert!(reachable.contains(&10));
+    assert!(reachable.contains(&11));
+    assert!(reachable.contains(&13));
+    assert!(reachable.contains(&14));
+  }
+
+  #[test]
   fn test_build_movement_graph() {
     let grid = vec![
       Cell::Hole, Cell::Empty, Cell::Empty, Cell::Hole,
@@ -311,65 +350,58 @@ mod test {
     ];
 
     let graph = grid_to_movement_graph(&grid, 4);
-    assert_eq!(graph.len(), 16);
+    assert_eq!(graph.len(), 8);
 
-    assert_eq!(graph[0].len(), 2);
-    assert_eq!(graph[0][0], 4);
-    assert_eq!(graph[0][1], 1);
+    assert!(!graph.contains_key(&0));
 
-    assert_eq!(graph[1].len(), 1);
-    assert_eq!(graph[1][0], 2);
+    assert!(graph.contains_key(&1));
+    assert_eq!(graph[&1].len(), 1);
+    assert_eq!(graph[&1][0], 2);
 
-    assert_eq!(graph[2].len(), 2);
-    assert_eq!(graph[2][0], 6);
-    assert_eq!(graph[2][1], 1);
+    assert!(graph.contains_key(&2));
+    assert_eq!(graph[&2].len(), 2);
+    assert_eq!(graph[&2][0], 6);
+    assert_eq!(graph[&2][1], 1);
 
-    assert_eq!(graph[3].len(), 1);
-    assert_eq!(graph[3][0], 2);
+    assert!(!graph.contains_key(&3));
 
-    assert_eq!(graph[4].len(), 0);
+    assert!(graph.contains_key(&4));
+    assert_eq!(graph[&4].len(), 0);
 
-    assert_eq!(graph[5].len(), 4);
-    assert_eq!(graph[5][0], 1);
-    assert_eq!(graph[5][1], 9);
-    assert_eq!(graph[5][2], 4);
-    assert_eq!(graph[5][3], 6);
+    assert!(!graph.contains_key(&5));
 
-    assert_eq!(graph[6].len(), 2);
-    assert_eq!(graph[6][0], 2);
-    assert_eq!(graph[6][1], 10);
+    assert!(graph.contains_key(&6));
+    assert_eq!(graph[&6].len(), 2);
+    assert_eq!(graph[&6][0], 2);
+    assert_eq!(graph[&6][1], 10);
 
-    assert_eq!(graph[7].len(), 2);
-    assert_eq!(graph[7][0], 11);
-    assert_eq!(graph[7][1], 6);
+    assert!(!graph.contains_key(&7));
 
-    assert_eq!(graph[8].len(), 2);
-    assert_eq!(graph[8][0], 4);
-    assert_eq!(graph[8][1], 9);
+    assert!(!graph.contains_key(&8));
 
-    assert_eq!(graph[9].len(), 2);
-    assert_eq!(graph[9][0], 13);
-    assert_eq!(graph[9][1], 10);
+    assert!(graph.contains_key(&9));
+    assert_eq!(graph[&9].len(), 2);
+    assert_eq!(graph[&9][0], 13);
+    assert_eq!(graph[&9][1], 10);
 
-    assert_eq!(graph[10].len(), 3);
-    assert_eq!(graph[10][0], 6);
-    assert_eq!(graph[10][1], 9);
-    assert_eq!(graph[10][2], 11);
+    assert!(graph.contains_key(&10));
+    assert_eq!(graph[&10].len(), 3);
+    assert_eq!(graph[&10][0], 6);
+    assert_eq!(graph[&10][1], 9);
+    assert_eq!(graph[&10][2], 11);
 
-    assert_eq!(graph[11].len(), 1);
-    assert_eq!(graph[11][0], 10);
+    assert!(graph.contains_key(&11));
+    assert_eq!(graph[&11].len(), 1);
+    assert_eq!(graph[&11][0], 10);
 
-    assert_eq!(graph[12].len(), 1);
-    assert_eq!(graph[12][0], 13);
+    assert!(!graph.contains_key(&12));
 
-    assert_eq!(graph[13].len(), 1);
-    assert_eq!(graph[13][0], 9);
+    assert!(graph.contains_key(&13));
+    assert_eq!(graph[&13].len(), 1);
+    assert_eq!(graph[&13][0], 9);
 
-    assert_eq!(graph[14].len(), 2);
-    assert_eq!(graph[14][0], 10);
-    assert_eq!(graph[14][1], 13);
+    assert!(!graph.contains_key(&14));
 
-    assert_eq!(graph[15].len(), 1);
-    assert_eq!(graph[15][0], 11);
+    assert!(!graph.contains_key(&15));
   }
 }
